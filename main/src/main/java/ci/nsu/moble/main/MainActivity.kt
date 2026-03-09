63fbfaa7c31903c6eab24a4063dcecf0e8db88e0
alp80 <kyrovad014@mail.ru>
2026-03-10 00:59:32 +0700

    Приложение готово, логика работает. Цвета можно использовать только из указанных в блоках.

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,18 +1,46 @@
 package ci.nsu.moble.main
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ci.nsu.moble.main.ui.main.MainFragment
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // Карта с разрешёнными цветами (название в нижнем регистре -> код цвета)
+    private val colorMap = mapOf(
+        "red" to Color.parseColor("#ff0000"),
+        "orange" to Color.parseColor("#ff7700"),
+        "yellow" to Color.parseColor("#FFFF00"),
+        "green" to Color.parseColor("#35e625"),
+        "blue" to Color.parseColor("#0335ff"),
+        "indigo" to Color.parseColor("#3e1d7d"),
+        "violet" to Color.parseColor("#8340ff")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val colorText = editText.text.toString().trim().lowercase()
+            if (colorText.isNotEmpty()) {
+                if (colorMap.containsKey(colorText)) {
+                    val color = colorMap[colorText]
+                    color?.let {
+                        button.setBackgroundColor(it)
+                    }
+                } else {
+                    Toast.makeText(this, "Этого цвета нет в списке, попробуйте ещё раз", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Введите цвет", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
